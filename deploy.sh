452b18007167e0059ac67533f69d15a075926503
Mathieu Morainville <mathieumorainville@hotmail.com>
2018-02-22 02:35:07 +0100

    chore: add a deploy script

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# abort the script if there is a non-zero error
+set -e
+
+# show where we are on the machine
+pwd
+
+npm install
+npm run test
+npm run build
+
+rm -rf ./docs/
+cd ./demo/
+
+npm install
+npm run e2e
+npm run build
+
+mv ./dist/ ../docs/
+cd ../
+cp ./public/demo.html ./dist
+
+echo "Finished Deployment!"
