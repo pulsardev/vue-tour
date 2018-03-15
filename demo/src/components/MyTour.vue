20db649d5efdeb2e308d7cd798866a0c8de6473d
Mathieu Morainville <mathieumorainville@hotmail.com>
2018-03-15 12:52:05 +0100

    chore: remove unneeded comment in CSS

--- demo/src/components/MyTour.vue
+++ demo/src/components/MyTour.vue
@@ -94,8 +94,7 @@ export default {
     transition: opacity .5s;
   }
 
-  .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */
-  {
+  .fade-enter, .fade-leave-to {
     opacity: 0;
   }
 </style>
