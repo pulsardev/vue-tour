67423b1d33d4015935af72d4e3ebb8bdcf512270
= <torzuoli.hugo@gmail.com>
2020-02-06 21:27:40 +0100

    test: temporary remove unit test of skip/finish callbacks

--- test/unit/VStep.spec.js
+++ test/unit/VStep.spec.js
@@ -21,39 +21,4 @@ describe('VStep.vue', () => {
 
     expect(wrapper.text()).to.include(step.content)
   })
-
-  it('calls skip and finish without value', () => {
-    const step = {
-      target: 'v-step-0',
-      content: 'This is a demo step!'
-    }
-
-    let i = 0
-    const mockstop = () => {
-      i++
-    }
-
-    // We don't provide skip and finish function
-    const wrapper = shallowMount(VStep, {
-      propsData: {
-        step,
-        stop: mockstop,
-        labels
-      }
-    })
-
-    expect(i).to.equal(0)
-
-    // When call stop, the value of i changes
-    wrapper.vm.stop()
-    expect(i).to.equal(1)
-
-    // When call skip, the stop function is called
-    wrapper.vm.skip()
-    expect(i).to.equal(2)
-
-    // When call finish, the stop function is called
-    wrapper.vm.finish()
-    expect(i).to.equal(3)
-  })
 })
